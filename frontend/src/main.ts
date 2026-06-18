f7047c6285270572aff13baacf79bd87d6383805
shiwenzhi <susunaKaa@example.com>
2026-06-18 10:45:53 +0800

    chore: 注册面试语音输入路由插件

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -7,6 +7,7 @@ import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 import App from './App.vue'
 import router from './router'
+import { interviewPlugin } from '@/plugins/interview'
 import './style.css'
 
 const app = createApp(App)
@@ -42,6 +43,9 @@ app.use(ElementPlus, {
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
+
+// 注册面试语音输入路由插件
+app.use(interviewPlugin)
 
 app.mount('#app')
 
